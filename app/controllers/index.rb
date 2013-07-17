102e2bac01822870e448ea16545578d094de76b0
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 16:11:33 -0500

    login and creation
    Added login and account creation...in progresss

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,40 @@
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+  # This page has create account in erb and login
+  # let's do two separate forms
 end
+
+post '/user' do
+  @user = User.authenticate(params[:user][:email], params[:user][:password])
+  if @user
+    session[:id] = @user.id
+    redirect to ("/secret")
+  else
+    #how do we make this a popup w/o using flash gem
+    error("password/email combination wrong/nonexistant") 
+    rest(2.0)
+    redirect to ("/")
+  end
+  #how does the session part work here
+  #redirect user to '/secret'
+end
+
+post '/create/user' do
+  #create user on this route
+  #return the user to '/'
+end
+
+get '/secret' do
+  if session[:id]
+    erb :secret
+  else
+    alert("you must be logged in to view this page")
+    redirect to ("/")
+  end
+end
+ 
+post '/secret' do
+  #this path sets the session.id = nil
+  #redirects to '/' when clicked
+end
+ #want to try the use you created in console?
